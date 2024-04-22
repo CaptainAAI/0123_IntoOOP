21550754e44a866b21d5268f998ae0fa7c9c35d8
CaptainAAI <anandaagungismail@gmail.com>
2024-04-22 13:33:35 +0700

    output procedures members for class matakuliah

--- ActivityOOP/ActivityOOP.cpp
+++ ActivityOOP/ActivityOOP.cpp
@@ -29,5 +29,12 @@ class Matakuliah {
 			cin >> sks;
 		}
 
+		void output() {
+			cout << "Kode MK: " << kodeMK << endl;
+			cout << "Nama MK: " << namaMK << endl;
+			cout << "SKS: " << sks << endl;
+		}
+
 
 };
+
